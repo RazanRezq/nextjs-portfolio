b721a60d0c71c331f98d8e830648e58fa0664bd2
Razan <razan.rezzq@gmail.com>
2025-02-15 21:58:09 +0300

    create new nextjs project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1 className="relative">Hello Portfolio</h1>
+      </div>
+    </main>
+  );
+}
