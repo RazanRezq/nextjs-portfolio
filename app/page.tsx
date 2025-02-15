31bb326bf585bc506134e4a784b660c89c31b177
Razan <razan.rezzq@gmail.com>
2025-02-15 22:00:24 +0300

    update homepage title to reflect new project details

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main>
       <div>
-        <h1 className="relative">Hello Portfolio</h1>
+        <h1 className="relative">
+          Hello Portfolio this is a new project using next js
+        </h1>
       </div>
     </main>
   );
