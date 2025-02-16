3e67d00d4203801b035bad29790d3a2c2c56eb3c
Razan <razan.rezzq@gmail.com>
2025-02-16 13:44:38 +0300

    add background gradient and styling to Hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,6 +15,14 @@ function Hero() {
         />
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
+
+      <div className="dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex h-screen w-full items-center justify-center bg-white dark:bg-black-100">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"></div>
+        <p className="relative z-20 bg-gradient-to-b from-neutral-200 to-neutral-500 bg-clip-text py-8 text-4xl font-bold text-transparent sm:text-7xl">
+          Backgrounds
+        </p>
+      </div>
     </div>
   );
 }
