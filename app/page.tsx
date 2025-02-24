cc1ed7f66c1607058c1fac4ac4c53a370067529f
Razan <razan.rezzq@gmail.com>
2025-02-24 16:34:08 +0300

    remove unused SVG files and add Grid component for enhanced layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Hero from '../components/Hero';
 import { FloatingNav } from '../components/ui/FloatingNav';
 import { FaHome } from 'react-icons/fa';
+import Grid from '../components/ui/Grid';
+import { BentoGrid } from '../components/ui/BentoGrid';
 export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 text-white sm:px-10">
@@ -9,6 +11,7 @@ export default function Home() {
           navItems={[{ name: 'Home', link: '/', icon: <FaHome /> }]}
         />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
