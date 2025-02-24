74f86386589de526aa801d7e51cc76c79079ecdf
Razan <razan.rezzq@gmail.com>
2025-02-24 12:48:46 +0300

    add FloatingNav component and integrate it into the Home page for improved navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import Hero from '../components/Hero';
+import { FloatingNav } from '../components/ui/FloatingNav';
+import { FaHome } from 'react-icons/fa';
 export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 text-white sm:px-10">
       <div className="w-full max-w-7xl">
+        <FloatingNav
+          navItems={[{ name: 'Home', link: '/', icon: <FaHome /> }]}
+        />
         <Hero />
       </div>
     </main>
