1ac030df44803882cc182b9827962c928eb9ef90
Razan <razan.rezzq@gmail.com>
2025-03-05 21:44:56 +0300

    migrate next.config.ts to next.config.mjs and update package dependencies

--- data/index.ts
+++ data/index.ts
@@ -52,7 +52,7 @@ export const gridItems = [
     title: 'Currently building a JS Animation library',
     description: 'The Inside Scoop',
     className: 'md:col-span-3 md:row-span-2',
-    imgClassName: 'absolute right-0 bottom-0 md:w-96 w-60',
+    imgClassName: 'absolute right-0 bottom-0 md:w-96 w-6 0',
     titleClassName: 'justify-center md:justify-start lg:justify-center',
     img: '/b5.svg',
     spareImg: '/grid.svg',
