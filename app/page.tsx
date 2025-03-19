3b2c8dd8cfea28d849370aea916bea74ee7d75ed
johnDoe <john.doe@example.com>
2025-03-19 03:53:35 +0300

    feat: update FloatingNav to use dynamic navItems and enhance RecentProjects layout with improved styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,12 @@ import { FaHome } from 'react-icons/fa';
 import Grid from '../components/Grid';
 import { BentoGrid } from '../components/ui/BentoGrid';
 import RecentProjects from '@/components/RecentProjects';
+import { navItems } from '@/data';
 export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 text-white sm:px-10">
       <div className="w-full max-w-7xl">
-        <FloatingNav
-          navItems={[{ name: 'Home', link: '/', icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
